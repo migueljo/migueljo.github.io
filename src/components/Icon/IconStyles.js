56cd5ad19c860dd6506f391a38f14232b3948549
Miguel Martínez <mmartinez@livevox.com>
2020-10-06 23:29:29 -0500

    WIP

---
+++ src/components/Icon/IconStyles.js
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled'
+
+export const Container = styled.div`
+    display: inline-block;
+    width: 20px;
+    height: 20px;
+
+    svg {
+        width: 20px;
+        height: 20px;
+    }
+`
